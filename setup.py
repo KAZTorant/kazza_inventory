c26e50df23a1e07fde2f5e2321a421be21d61bf9
Zakir Aghayev <zakir.aghayev.19@gmail.com>
2025-03-12 15:32:25 +0400

    done

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='inventory',           
-    version='1.0.0',
+    version='1.0.1',
     description='KAZZA inventory management system',
     author='Zakir Aghayev.',
     author_email='zakir-aghayev@outlook.com',
